1c2d77ae592d3c9076ab3ce941447d8ffc02952e
Heron Yang <heron.yang.tw@gmail.com>
2020-08-02 23:26:09 -0700

    Update incorrect comment of car/bike.

--- creational/factory/main.cc
+++ creational/factory/main.cc
@@ -7,17 +7,17 @@
 static const int kBikeWheelSize = 10;
 
 void startCarWorld() {
-  std::cout << "[START BIKE WORLD]" << std::endl;
+  std::cout << "[START CAR WORLD]" << std::endl;
 
   CarFactory car_factory;
   World world(car_factory);
   world.start();
 
-  std::cout << "[END BIKE WORLD]" << std::endl;
+  std::cout << "[END CAR WORLD]" << std::endl;
 }
 
 void startBikeWorld() {
-  std::cout << "[START CAR WORLD]" << std::endl;
+  std::cout << "[START BIKE WORLD]" << std::endl;
 
   BikeFactory bike_factory(kBikeWheelSize);
   World world(bike_factory);
