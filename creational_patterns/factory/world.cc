37f2bf655889f0afa1a80b34bdd39068ebb5d6ba
Heron Yang <heron.yang.tw@gmail.com>
2020-08-09 13:39:20 -0700

    Remove old files.

--- creational_patterns/factory/world.cc
+++
@@ -1,8 +0,0 @@
-#include "world.h"
-#include "vehicle_factory.h"
-
-World::World(VehicleFactory &vehicle_factory) {
-  vehicle_ = vehicle_factory.create();
-}
-
-void World::start() { vehicle_->move(); }
